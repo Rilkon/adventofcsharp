603675971c5c3f760f3f10b3d7d808f06702109d
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-29 23:12:46 +0100

    day02 p1 + p2

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -13,12 +13,64 @@ public class Day02 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        return new ("0");
+        int sum = 0;
+        foreach (string line in this._input.Split("\n"))
+        {
+
+            int currmax = int.MinValue;
+            int currmin = int.MaxValue;
+
+            foreach (string el in line.Split("\t"))
+            {
+                int curr = int.Parse(el);
+                currmax = Math.Max(curr, currmax);
+                currmin = Math.Min(curr, currmin);
+            }
+            sum += currmax - currmin;
+
+        }
+
+        return new("" + sum);
     }
 
 
     public override ValueTask<string> Solve_2()
     {
-        return new ("0");
+        int sum = 0;
+        foreach (string line in this._input.Split("\n"))
+        {
+
+            int divident = 0;
+            int divisor = 1;
+            foreach (string first in line.Split("\t"))
+            {
+                int curr = int.Parse(first);
+
+                foreach (string second in line.Split("\t"))
+                {
+                    int nx = int.Parse(second);
+
+                    if (curr == nx) continue;
+
+                    if (curr % nx == 0)
+                    {
+                        divident = curr;
+                        divisor = nx;
+                        break;
+                    }
+
+                    if (nx % curr == 0)
+                    {
+                        divident = nx;
+                        divisor = curr;
+                    }
+                }
+            }
+            sum += divident / divisor;
+
+        }
+
+
+        return new("" + sum);
     }
 }
