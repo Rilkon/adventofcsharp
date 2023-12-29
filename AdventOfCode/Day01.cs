44121d5051a337ead3be355593c41be1b3e09fa9
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-29 22:13:24 +0100

    2017 Day01 p1 and p2, now with more LINQ

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -9,34 +9,26 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    private int InverseCaptcha(int[] digits, bool p2 = false)
-    {
-        int sum = 0;
-        for (int i = 0; i < digits.Length; i++)
+    private int InverseCaptcha(bool p2 = false)
+    {    
+        int[] digits = this._input.Select(c => int.Parse(c.ToString())).ToArray();
+        return digits.Select((int curr, int i) =>
         {
-
-            int index = (i + 1) % digits.Length;
-            if (p2) index = (i + (digits.Length / 2)) % digits.Length;
-            if (digits[i] == digits[index])
-            {
-                sum += digits[i];
-            }
-        }
-        return sum;
+            int index = p2 ? (i + (digits.Length / 2)) % digits.Length : (i + 1) % digits.Length;
+            return curr == digits[index] ? curr : 0;
+        }).Sum();
     }
 
     public override ValueTask<string> Solve_1()
     {
-        int[] digits = this._input.Select(c => int.Parse(c.ToString())).ToArray();
-        return new($"Solution to {ClassPrefix} {CalculateIndex()} Part 1: {InverseCaptcha(digits, false)}");
+        return new (""+InverseCaptcha());
     }
-
 
 
     public override ValueTask<string> Solve_2()
     {
         int[] digits = this._input.Select(c => int.Parse(c.ToString())).ToArray();
-        return new($"Solution to {ClassPrefix} {CalculateIndex()} Part 2: {InverseCaptcha(digits, true)}");
+        return new (""+InverseCaptcha(true));
 
 
     }
