ab97b6197fcd3c662b14accf47b87d0c2db45b30
Rilko <d.pfauntsch@gmail.com>
2023-12-29 19:34:12 +0100

    Advent of Code Csharp 2017 day01

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -9,7 +9,35 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private int InverseCaptcha(int[] digits, bool p2 = false)
+    {
+        int sum = 0;
+        for (int i = 0; i < digits.Length; i++)
+        {
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+            int index = (i + 1) % digits.Length;
+            if (p2) index = (i + (digits.Length / 2)) % digits.Length;
+            if (digits[i] == digits[index])
+            {
+                sum += digits[i];
+            }
+        }
+        return sum;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int[] digits = this._input.Select(c => int.Parse(c.ToString())).ToArray();
+        return new($"Solution to {ClassPrefix} {CalculateIndex()} Part 1: {InverseCaptcha(digits, false)}");
+    }
+
+
+
+    public override ValueTask<string> Solve_2()
+    {
+        int[] digits = this._input.Select(c => int.Parse(c.ToString())).ToArray();
+        return new($"Solution to {ClassPrefix} {CalculateIndex()} Part 2: {InverseCaptcha(digits, true)}");
+
+
+    }
 }
